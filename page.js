caf99f54e0d22b5da58d9ac01aac7c7c5a344acb
Jan Kuehle <jkuehle90@gmail.com>
2017-10-25 22:17:39 +0200

    Make request to absolute extension URL to make issue clearer

--- page.js
+++ page.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const spanRequestedUrl = document.getElementById('requested-url');
     const spanResponseUrl = document.getElementById('response-url');
 
-    const requestUrl = '/manifest.json';
+    const requestUrl = chrome.extension.getURL('/manifest.json');
     const xhr = new XMLHttpRequest();
     xhr.open('GET', requestUrl, true);
     xhr.onloadend = function () {
